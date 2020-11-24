ca9ab9ed1babbd4db73f492b38ca4c0171d3a2e0
Mladen Srdić <m.s.qubits@gmail.com>
2020-11-24 21:38:03 +0100

    Simple authenticated search query.

--- src/HypothesisClient.hs
+++ src/HypothesisClient.hs
@@ -4,13 +4,30 @@ module HypothesisClient (main) where
 import Data.Configurator (Worth(Required), load)
 import qualified Data.Configurator as DC
 
-import Data.Text ( Text )
+import Data.Text ( Text, concat )
+import Data.Text.Encoding ( encodeUtf8 )
 import System.FilePath ((</>), (<.>))
+
+import Network.Wreq ( getWith, defaults, param, header)
+import Control.Lens ((.~), (&))
 
 _getAuthToken :: FilePath -> IO (Maybe Text)
 _getAuthToken path = do
   config <- load [Required path]
   DC.lookup config "token" :: IO (Maybe Text)
 
+getAuthHeader :: Maybe Text -> IO Text
+getAuthHeader t =
+  return $ case t of
+    Nothing -> error "Token not set in auth.config."
+    Just token -> Data.Text.concat ["Bearer ", token]
+
 main :: IO ()
-main = _getAuthToken ("conf" </> "auth" <.> "config") >>= print
+main = do
+  token <- _getAuthToken ("conf" </> "auth" <.> "config")
+  authHeader <- getAuthHeader token
+  let opts = defaults & param "user" .~ ["acct:msrdic@hypothes.is"]
+                      & param "group" .~ ["__world__"]
+                      & header "Authorization" .~ [encodeUtf8 authHeader]
+  r <- getWith opts "https://api.hypothes.is/api/search"
+  print r
